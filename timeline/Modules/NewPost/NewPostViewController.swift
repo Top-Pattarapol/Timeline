c4860a4609940430242bfc19acc2486c7016cffb
pattarapol sawasdee <pattarapol.s@kbtg.tech>
2019-10-14 21:11:32 +0700

    handle show newpost

--- timeline/Modules/NewPost/NewPostViewController.swift
+++ timeline/Modules/NewPost/NewPostViewController.swift
@@ -66,15 +66,55 @@ class NewPostViewController: UIViewController, NewPostDisplayLogic
   
   // MARK: View lifecycle
   
-  override func viewDidLoad()
-  {
+  override func viewDidLoad() {
     super.viewDidLoad()
+    shouldHideKeyboardWhenTappedAnyArea()
+    navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: "Close", style: .done, target: self, action: #selector(close))
+    navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "Create", style: .done, target: self, action: #selector(create))
+    setupView()
     doSomething()
+
   }
   
   // MARK: Do something
   
   //@IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet var inputTextView: UITextView!
+  @IBOutlet var image1: UIImageView!
+  @IBOutlet var image2: UIImageView!
+  @IBOutlet var image3: UIImageView!
+  @IBOutlet var addImage1: UIButton!
+  @IBOutlet var addImage2: UIButton!
+  @IBOutlet var addImage3: UIButton!
+  var imagePicker: ImagePicker!
+  var itemImageSelect: UIButton?
+  var valuePlacehoder = " What's on your mind?  "
+  
+  @IBAction func action1(_ sender: Any) {
+    itemImageSelect = addImage1
+    imagePicker.present(from: sender as! UIView)
+  }
+  @IBAction func action2(_ sender: Any) {
+    itemImageSelect = addImage2
+    imagePicker.present(from: sender as! UIView)
+  }
+  @IBAction func action3(_ sender: Any) {
+    itemImageSelect = addImage3
+    imagePicker.present(from: sender as! UIView)
+  }
+
+  func setupView() {
+    inputTextView.applyBorderConner(radius: 16, color: .darkGray)
+    addImage1.applyBorderConner(radius: 0, color: .systemBlue)
+    addImage2.applyBorderConner(radius: 0, color: .systemBlue)
+    addImage3.applyBorderConner(radius: 0, color: .systemBlue)
+    addImage2.isHidden = true
+    addImage3.isHidden = true
+    imagePicker = ImagePicker(presentationController: self, delegate: self)
+    inputTextView.delegate = self
+    inputTextView.text = valuePlacehoder
+    inputTextView.textColor = UIColor.lightGray
+  }
   
   func doSomething()
   {
@@ -85,5 +125,50 @@ class NewPostViewController: UIViewController, NewPostDisplayLogic
   func displaySomething(viewModel: NewPost.Something.ViewModel)
   {
     //nameTextField.text = viewModel.name
+  }
+
+  @objc func create() {
+
+  }
+
+  @objc func close() {
+    self.navigationController?.popViewController(animated: true)
+  }
+}
+
+extension NewPostViewController: ImagePickerDelegate {
+  func didSelect(image: UIImage?) {
+    switch itemImageSelect {
+    case addImage1:
+      image1.image = image
+      addImage1.isHidden = true
+      addImage2.isHidden = false
+    case addImage2:
+      image2.image = image
+      addImage2.isHidden = true
+      addImage3.isHidden = false
+    case addImage3:
+      image3.image = image
+      addImage3.isHidden = true
+    default:
+      break
+    }
+  }
+}
+
+extension NewPostViewController: UITextViewDelegate {
+
+  func textViewDidBeginEditing(_ textView: UITextView) {
+      if textView.textColor == UIColor.lightGray {
+          textView.text = nil
+          textView.textColor = UIColor.black
+      }
+  }
+
+  func textViewDidEndEditing(_ textView: UITextView) {
+      if textView.text.isEmpty {
+          textView.text = valuePlacehoder
+          textView.textColor = UIColor.lightGray
+      }
   }
 }
