24eb63a50867326b54fc5ff4c1f92150f22bd43c
pattarapol sawasdee <pattarapol.s@kbtg.tech>
2019-10-14 11:00:00 +0700

    add UIViewController extenion

---
+++ timeline/Extension/UIVIewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension UIViewController {
+  func shouldHideKeyboardWhenTappedAnyArea() {
+      let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+      tap.cancelsTouchesInView = false
+      view.addGestureRecognizer(tap)
+  }
+
+  @objc func hideKeyboard() {
+      view.endEditing(true)
+  }
+}
